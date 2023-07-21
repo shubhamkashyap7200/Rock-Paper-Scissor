46bfb2d9ca27b9abc2240bfec9f40b1d630c0535
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2023-07-21 22:07:27 +0530

    Initial Commit

---
+++ Rock Paper Scissor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissor
+//
+//  Created by Shubham on 21/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rock Paper Scissor/Rock_Paper_ScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorApp.swift
+//  Rock Paper Scissor
+//
+//  Created by Shubham on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
